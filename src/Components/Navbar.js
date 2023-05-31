23a42073ea40a88adfa43781dd8dee6eb50a12c3
DanielleM <dlmedeiro@gmail.com>
2023-05-31 09:51:36 -0700

    remove user tooltip for now

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -249,13 +249,13 @@ export default function Navbar() {
           </Typography>
           {isLoggedIn === true ? loggedInLg() : loggedOutLg()}
 
-          <Box sx={{ flexGrow: 0 }}>
+          {/* <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
             </Tooltip>
-          </Box>
+          </Box> */}
         </Toolbar>
       </Container>
     </AppBar>
